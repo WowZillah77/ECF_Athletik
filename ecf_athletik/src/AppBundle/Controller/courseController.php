06c9f0a13a6e35a5d1900337663d7636e9c3a0e5
WowZillah77 <wowzillah77@gmail.com>
2017-07-20 15:08:34 +0200

    DataBase for Anonymous Log now display properly

--- ecf_athletik/src/AppBundle/Controller/courseController.php
+++ ecf_athletik/src/AppBundle/Controller/courseController.php
@@ -17,7 +17,8 @@ class courseController extends Controller
 
         $id=$request->query->get('id');
         $em = $this->getDoctrine()->getManager();
-        $meetingName = $em->getRepository("AppBundle:Meeting")->findAll();
+        $meeting = $em->getRepository("AppBundle:Meeting");
+        $meetingName= $meeting->findBy(array('id'=> $id));
         $repository=$em->getRepository("AppBundle:Result");
         $athletes = $repository->findBy( array('meeting'=> $id), array('points'=>'DESC'));
 
@@ -29,8 +30,14 @@ class courseController extends Controller
      */
     public function classementCourseAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('page/classement.html.twig');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $sql='SELECT SUM(result.points) as total, athlete.lastname, athlete.firstname FROM result inner join athlete on result.athlete_id = athlete.id inner join meeting on result.meeting_id = meeting.id WHERE YEAR(CURRENT_DATE()) = 2017 GROUP BY athlete.id ORDER BY total DESC 
+';      $toto=$em->getConnection()->prepare($sql);
+        $toto->execute();
+        $resultat=$toto->fetchAll();
+        return $this->render('page/classement.html.twig',['classement'=>$resultat]);
 
     }
 
